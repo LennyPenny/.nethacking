ffbddff1c18e2d9575bf0fba3e846fbfa551e92d
Lennart Bernhardt <l.bernhardt@live.de>
2016-09-28 07:25:05 -0700

    Slides und beispielprogramme hinzugefügt

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crackme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tries = 0;
+
+            while (Console.ReadLine() != "kassel")
+            {
+                Console.WriteLine($"Wrong password, you tried {++tries} {(tries == 1 ? "time" : "times")}");
+                Console.WriteLine("Try again");
+            }
+
+            Console.WriteLine("Success!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program_async.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace crackme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crackmeTask = CrackMe();
+            while (!crackmeTask.IsCompleted)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        private static async Task CrackMe()
+        {
+            var tries = 0;
+
+            while (Console.ReadLine() != "kassel")
+            {
+                Console.WriteLine($"Wrong password, you tried {++tries} {(tries == 1 ? "time" : "times")}");
+                Console.WriteLine("Try again");
+
+                await Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                });
+            }
+
+            Console.WriteLine("Success!");
+
+            Console.ReadKey();
+        }
+    }
+}
